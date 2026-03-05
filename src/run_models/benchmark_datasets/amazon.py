5c21dad37417826eff908b18a820f503fe4e7bbf
harshil0217 <harshil0217@gmail.com>
2026-03-05 11:40:59 -0500

    pre cuml refactoring

--- src/run_models/benchmark_datasets/amazon.py
+++ src/run_models/benchmark_datasets/amazon.py
@@ -377,6 +377,7 @@ scores_df = pd.DataFrame(rows)
 
 # Optional: sort for easier viewing
 scores_df = scores_df.sort_values(by=["embedding_model", "reduction_method", "cluster_method", "level"]).reset_index(drop=True)
+os.makedirs("results", exist_ok=True)
 scores_df.to_csv(f"results/amazon_clustering_scores.csv", index=False)
 
 combo_color_map = {
